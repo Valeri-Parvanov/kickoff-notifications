c46cef2b8436f7bf97bbcc678eaf09bb26a1c987
Valeri-Parvanov <valyo.parvanov@gmail.com>
2026-07-17 22:13:21 +0200

    feat: add exception handling and scheduled cleanup job for notifications

---
+++ src/main/java/bg/softuni/kickoffnotifications/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package bg.softuni.kickoffnotifications.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
